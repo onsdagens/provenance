5699689aa4ddb4bd32ae2d3b11b009d2094756ce
onsdagens <pawel.dzialo@ltu.se>
2025-04-02 17:38:11 +0200

    small simplification

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,12 @@ fn main() {
     let c = {
         let v = 0;
         &v as *const _ as usize
-    };
+    }; // usize
     let d = {
         let v = 0;
         &v as *const _ as usize
-    };
-    let e = (c - d) as usize;
+    }; // usize
+    let e = c - d; // (usize - usize = usize)
     println!("{}==0:{}", e, e == 0);
     println!("{}==0:{}", e, e == 0);
 }
